bee1395766ec2754e2828d9f5c450b50f84b6091
Metin Nergiz <m.nergiz@arcusx.com>
2015-08-24 15:53:15 +0200

    Works with 5 and 3

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -2,16 +2,29 @@ package fizzbuzz;
 
 public class FizzBuzz {
 	public static void main(String[] args) {
+
 		int x = 0;
 		while (x < 100) {
 			x++;
 
-			System.out.println(x);
-			// int modulo = x % 3;
-			// System.out.println("Modulo 3: " + modulo);
-			// if (i) {
-			// sytem.out.println ("fizz");
-		}
+			// int rest35 = (x%15);
+			// if (rest35==0)
+			// System.out.println("fizz&buzz");
+			// }
 
+			int rest3 = (x % 3);
+			if (rest3 == 0) {
+				System.out.println("fizz");
+			} else {
+
+				int rest5 = (x % 5);
+				if (rest5 == 0) {
+					System.out.println("buzz");
+				} else {
+					System.out.println(x);
+
+				}
+			}
+		}
 	}
 }
