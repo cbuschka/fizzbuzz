3f57fc46f08a05bd6193cda7657d5d9f646f1526
Metin Nergiz <m.nergiz@arcusx.com>
2015-08-24 12:06:54 +0200

    FizzBuzz for 1 to 100.

---
+++ src/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,17 @@
+package fizzbuzz;
+
+public class FizzBuzz {
+	public static void main(String[] args) {
+		int x = 0;
+		while (x < 100) {
+			x++;
+
+			System.out.println(x);
+			// int modulo = x % 3;
+			// System.out.println("Modulo 3: " + modulo);
+			// if (i) {
+			// sytem.out.println ("fizz");
+		}
+
+	}
+}
